cca9c70e2441f9c6dddf3c53159ddcdba0f76460
Ridoyb <ridoyb8730@gmail.com>
2022-11-25 04:09:38 +0600

    Banner added in home page

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold"> </h1>
+                <h1 className="mb-5 text-5xl font-bold">MAKE YOUR BIKE TRULY YOURS</h1>
                 <p className="mb-5">Buy and sell your bikes</p>
                 </div>
             </div>
